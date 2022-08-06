0c3085de357a30f5b004d6b4d0e4d558fdb0b442
CAMPUS\venkata.ramana <venkatar@interfile.co.za>
2022-08-06 19:48:37 +0200

    jenkinsfile

---
+++ SpingBootKubernetes/src/main/java/com/example/spingbootkubernetes/SpingBootKubernetesApplication.java
@@ -0,0 +1,21 @@
+package com.example.spingbootkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpingBootKubernetesApplication {
+    @GetMapping("/")
+    public String home(){
+        return " Congratulations you successfully deployed your Application into Kubernetes------!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootKubernetesApplication.class, args);
+    }
+
+}
+
---
+++ SpingBootKubernetes/src/test/java/com/example/spingbootkubernetes/SpingBootKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spingbootkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
